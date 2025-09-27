c4dbabce768d524ca8571b8fbbee50d395e2c678
Omid Fathollahzadeh <omid.fathollahzadeh@SF-13265.local>
2025-09-27 15:33:39 +0330

    [feat] append new images

--- internal/service/promptService.go
+++ internal/service/promptService.go
@@ -45,24 +45,38 @@ func (s promptService) Generate(ctx *gin.Context, selectedImages []string, servi
 	if err != nil {
 		return post, balance, err
 	}
+	lastSelectedImages := post.GetSelectedImages()
+	imagesLen := 0
 
-	imagesLen := len(selectedImages)
+	for _, imageUrl := range selectedImages {
+		if s.contains(lastSelectedImages, imageUrl) {
+			continue
+		}
+		imagesLen++
+	}
 
 	if imagesLen == 0 {
 		return post, balance, nil
 	}
-
 	if imagesLen > balance {
 		return post, balance, errors.New("not enough token")
 	}
+
+	lastNewImages := post.GetNewImages()
+
 	var outputs []string
 	for _, imageUrl := range selectedImages {
+		if s.contains(lastSelectedImages, imageUrl) {
+			continue
+		}
 		o, generateErr := s.promptRepo.Generate(ctx, imageUrl)
 		if generateErr != nil {
 			continue
 		}
 		outputs = append(outputs, o)
 	}
+
+	outputs = append(outputs, lastNewImages...)
 
 	user.Balance -= imagesLen
 
@@ -76,4 +90,14 @@ func (s promptService) Generate(ctx *gin.Context, selectedImages []string, servi
 	s.postService.UpdatePost(post)
 
 	return post, user.Balance, nil
+}
+
+func (s promptService) contains(sl []string, str string) bool {
+	for _, v := range sl {
+		if v == str {
+			return true
+		}
+	}
+
+	return false
 }
